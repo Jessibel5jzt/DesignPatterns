3b295730ac9ba94fdbca4b5181becb6f713acc25
Jessibel <jessibel_jzt@163.com>
2020-01-17 17:19:36 +0800

    BuilderMode.csproj SingletonMode.csproj FactoryMethodMode.csproj

---
+++ FactoryMethodMode/FactoryMethodMode/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FactoryMethodMode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var theCreatorGM = new ConcreteCreator();
+			var theProductA = theCreatorGM.FactoryMethod<ConcreteProductA>();
+			var theProductB = theCreatorGM.FactoryMethod<ConcreteProductB>();
+			Console.ReadKey();
+		}
+
+	}
+
+	public class ConcreteProductA : Product
+	{
+		public ConcreteProductA()
+		{
+			Console.WriteLine("生成对象类A");
+		}
+	}
+
+	public class ConcreteProductB : Product
+	{
+		public ConcreteProductB()
+		{
+			Console.WriteLine("生成对象类B");
+		}
+	}
+	public abstract class Product { }
+
+	interface ICreator
+	{
+		Product FactoryMethod<T>() where T : Product, new();
+	}
+
+	public class ConcreteCreator : ICreator
+	{
+		public ConcreteCreator()
+		{
+			Console.WriteLine("产生具体工厂：ConcreteCreator");
+		}
+
+		public Product FactoryMethod<T>() where T : Product, new()
+		{
+			return new T();
+		}
+	}
+
+}
---
+++ SingletonMode/SingletonMode/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SingletonMode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Singleton.Ins.Hahaha();
+			Singleton.Ins.Heiheihei();
+			Singleton.Ins.Wuwuwu();
+
+			Console.WriteLine("FINAL VALUE == " + Singleton.Ins.IntA);
+			Console.ReadKey();
+		}
+	}
+
+
+	class Singleton
+	{
+		public static Singleton Ins { get { return GetInstance(); } private set { } }
+
+		private static Singleton _instance = null;
+		private static Singleton GetInstance()
+		{
+			if (_instance == null)
+			{
+				_instance = new Singleton();
+				Console.WriteLine("Go Singleton");
+			}
+			return _instance;
+		}
+
+		private Singleton()
+		{
+		}
+
+		public int IntA { get; private set; }
+		public void Hahaha()
+		{
+			IntA++;
+			Console.WriteLine("Hahaha ——" + IntA);
+		}
+
+		public void Heiheihei()
+		{
+			IntA++;
+			Console.WriteLine("Heiheihei ——" + IntA);
+		}
+		public void Wuwuwu()
+		{
+			IntA++;
+			Console.WriteLine("Wuwuwu ——" + IntA);
+		}
+	}
+
+}
