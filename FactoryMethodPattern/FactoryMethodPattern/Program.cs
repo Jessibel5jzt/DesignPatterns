64b6e2e31194c4dd80ccebf4077605ae98fbc143
Jessibel <jessibel_jzt@163.com>
2020-01-17 18:02:48 +0800

    Rename

--- FactoryMethodPattern/FactoryMethodPattern/Program.cs
+++ FactoryMethodPattern/FactoryMethodPattern/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace FactoryMethodMode
+namespace FactoryMethodPattern
 {
 	class Program
 	{
--- SingletonPattern/SingletonPattern/Properties/AssemblyInfo.cs
+++ SingletonPattern/SingletonPattern/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SingletonMode")]
+[assembly: AssemblyTitle("SingletonPattern")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SingletonMode")]
+[assembly: AssemblyProduct("SingletonPattern")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
