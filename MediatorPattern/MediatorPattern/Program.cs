67411bd10da8e05dc388adfbe93e9e53414e69e2
Jessibel <jessibel_jzt@163.com>
2020-01-20 11:18:42 +0800

    MediatorPattern.csproj StatePattern.csproj

---
+++ MediatorPattern/MediatorPattern/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cm = new ConcreteMediator();
+			var c1 = new ConcreateColleague1(cm);
+			var c2 = new ConcreateColleague2(cm);
+
+			cm.SetColleague1(c1);
+			cm.SetColleague2(c2);
+			c1.Action();
+			c2.Action();
+			Console.ReadKey();
+		}
+	}
+
+	public abstract class Mediator
+	{
+		public abstract void SendMessage(Colleague theColleague, string Message);
+	}
+
+	public abstract class Colleague
+	{
+		protected Mediator m_Mediator = null;
+
+		public Colleague(Mediator theMediator)
+		{
+			m_Mediator = theMediator;
+		}
+
+		public abstract void Request(string Message);
+	}
+
+	public class ConcreateColleague1 : Colleague
+	{
+		public ConcreateColleague1(Mediator theMediator) : base(theMediator) { }
+
+		public void Action()
+		{
+			m_Mediator.SendMessage(this, "Colleage1发出通知");
+		}
+
+		public override void Request(string Message)
+		{
+			Console.WriteLine("ConcreateColleague1.Request:" + Message);
+		}
+	}
+
+	public class ConcreateColleague2 : Colleague
+	{
+		public ConcreateColleague2(Mediator theMediator) : base(theMediator) { }
+
+		public void Action()
+		{
+			m_Mediator.SendMessage(this, "Colleage2发出通知");
+		}
+
+		public override void Request(string Message)
+		{
+			Console.WriteLine("ConcreateColleague2.Request:" + Message);
+		}
+	}
+
+	public class ConcreteMediator : Mediator
+	{
+		ConcreateColleague1 m_Colleague1 = null;
+		ConcreateColleague2 m_Colleague2 = null;
+
+		public void SetColleague1(ConcreateColleague1 theColleague)
+		{
+			m_Colleague1 = theColleague;
+		}
+
+		public void SetColleague2(ConcreateColleague2 theColleague)
+		{
+			m_Colleague2 = theColleague;
+		}
+
+		public override void SendMessage(Colleague theColleague, string Message)
+		{
+			if (m_Colleague1 == theColleague)
+				m_Colleague2.Request(Message);
+
+			if (m_Colleague2 == theColleague)
+				m_Colleague1.Request(Message);
+		}
+	}
+}
---
+++ StatePattern/StatePattern/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Context theContext = new Context();
+			theContext.SetState(new ConcreteStateA(theContext));
+			theContext.Request(5);
+			theContext.Request(15);
+			theContext.Request(25);
+			theContext.Request(35);
+			Console.ReadLine();
+		}
+	}
+
+	public class Context
+	{
+		State m_State = null;
+
+		public void Request(int Value)
+		{
+			m_State.Handle(Value);
+		}
+
+		public void SetState(State theState)
+		{
+
+			Console.WriteLine("Context.SetState:" + theState);
+			m_State = theState;
+		}
+	}
+
+	public abstract class State
+	{
+		protected Context m_Context = null;
+		public State(Context theConText)
+		{
+			m_Context = theConText;
+		}
+		public abstract void Handle(int Value);
+	}
+
+	//状态A
+	public class ConcreteStateA : State
+	{
+		public ConcreteStateA(Context theContext) : base(theContext) { }
+
+		public override void Handle(int Value)
+		{
+			Console.WriteLine("ConcreteStateA.Handle");
+			if (Value > 10)
+				m_Context.SetState(new ConcreteStateB(m_Context));
+		}
+	}
+
+	//状态B
+	public class ConcreteStateB : State
+	{
+		public ConcreteStateB(Context theContext) : base(theContext) { }
+
+		public override void Handle(int Value)
+		{
+			Console.WriteLine("ConcreteStateA.Handle");
+			if (Value > 20)
+				m_Context.SetState(new ConcreteStateC(m_Context));
+		}
+	}
+
+	//状态C
+	public class ConcreteStateC : State
+	{
+		public ConcreteStateC(Context theContext) : base(theContext) { }
+
+		public override void Handle(int Value)
+		{
+			Console.WriteLine("ConcreteStateA.Handle");
+			if (Value > 30)
+				m_Context.SetState(new ConcreteStateA(m_Context));
+		}
+	}
+
+
+}
